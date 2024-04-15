01f9fd6bc56083fc2758358d59f529e268cde506
Gustav-MP <gustavo.gutierrez@movipro.cl>
2024-04-15 18:48:14 -0400

    [RMV] removed first tests components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
-import { FirstFunxxxComponent } from './first-funxxx/first-funxxx.component';
 
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatListModule } from '@angular/material/list';
@@ -8,7 +7,7 @@ import { MatListModule } from '@angular/material/list';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, FirstFunxxxComponent, MatGridListModule, MatListModule],
+  imports: [RouterOutlet, RouterModule, MatGridListModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

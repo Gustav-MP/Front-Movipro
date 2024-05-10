4aae8db0e2f74ad049119febaaf532c999be92ab
Gustav-MP <gustavo.gutierrez@movipro.cl>
2024-05-10 06:01:56 -0400

    [ADD] local support for currency CLP in pipes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 
+import { registerLocaleData } from '@angular/common';
+import localeEsCL from '@angular/common/locales/es-CL';
+
+registerLocaleData(localeEsCL);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'es-CL' },
   ],
 };

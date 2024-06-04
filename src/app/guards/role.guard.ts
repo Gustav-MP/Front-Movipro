1b05533bc778f66426fb71ace14ea465b1bb3fb2
Gustav-MP <gustavo.gutierrez@movipro.cl>
2024-06-04 08:02:35 -0400

    [FIX] resolve problems with role guard

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -15,11 +15,16 @@ export const roleGuard: CanActivateFn = async (
   const router = inject(Router);
 
   const expectedRoles = route.data['roles'] as Array<string>;
-  const userRole: string = await authService.userRole();
 
-  if (expectedRoles.includes(userRole)) {
-    return true;
-  } else {
+  try {
+    const userRole = await authService.userRole();
+    const hasRole = expectedRoles.includes(userRole);
+    if (!hasRole) {
+      router.navigate(['/login']);
+    }
+    return hasRole;
+  } catch (error) {
+    console.error('Error checking roles:', error);
     router.navigate(['/login']);
     return false;
   }
